0510a32cce5936bd6c624130f4fac5312ddda85c
ButtercupPug <148472900+ButtercupPug@users.noreply.github.com>
2023-11-14 13:08:25 -0700

    Update functions.js

--- public_html/functions.js
+++ public_html/functions.js
@@ -30,11 +30,25 @@ function showContent(type)
     document.getElementById('content').innerHTML = content;
 }
 
-function popUp()
+function showChannel(type)
 {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+    var channel = "";
+
+    switch (type) {
+        case 'channel1':
+            channel = "Channel 1";
+            break;
+        case 'channel2':
+            channel = "Channel 2";
+            break;
+
+        default:
+            channel = "Default Channel";
+    }
+
+    document.getElementById('channel').innerHTML = channel;
 }
+
 
 
 
@@ -43,12 +57,20 @@ function getEditProfileContent() {
     <div class="settings>
         <div id="profileContent">
             <h2>Edit Profile</h2>
+            <span class="dot></span>
+
             <form>
                 <label for="username">Username:</label>
                 <input type="text" id="username" name="username"><br>
+
+                <label for="displayName">Display Name:</label>
+                <input type="text" id="displayName" name="displayName"><br>
             
                 <label for="email">Email:</label>
                 <input type="email" id="email" name="email"><br>
+
+                <label for="birthday">Birthday:</label>
+                <input type="date" id="birthday" name="birthday"><br>
             
                 <!-- Add more form fields as needed -->
             
@@ -67,6 +89,10 @@ function getDisplayContent() {
             <label>
                 <input type="checkbox" id="darkMode"> Dark Mode
             </label><br>
+
+            <label for="color">Color Scheme: </label>
+            <input type="color" id="color" name="color"><br>
+
             
             <!-- Add more display settings as needed -->
             
@@ -84,6 +110,9 @@ function getNotificationContent()
         <label>
             <input type="checkbox" id="notifications"> Notifications Enabled
         </label><br>
+
+        <label for="timeout"> Notification Time Out: </label>
+        <input type="time" id="timeout" name="timeout"><br>
         
         <!-- Add more display settings as needed -->
         
