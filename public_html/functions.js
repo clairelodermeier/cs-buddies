614dcd244ae214f0f2f726f203c229e619c3bcde
ButtercupPug <148472900+ButtercupPug@users.noreply.github.com>
2023-11-16 13:03:46 -0700

    Update functions.js
    Moved all HTML from other functions into settings. Still struggling regarding the functionality of clicking the different tabs and the different HTML content shows. Currently just using npx to run a local server with the files to see how I can fix it.
    
    ~Audrey

--- public_html/functions.js
+++ public_html/functions.js
@@ -4,164 +4,39 @@ function addStyleSheet(sheetName) {
         document.getElementById('cssFile').href = sheetName;
 }
 
+
 //To display different settings depending on the button
 function showContent(type)
 {
-    var content = "";
+    fetch("./settings.html")
+    .then(response => response.text())
+    .then(data => {
+        switch(type)
+        {
+            case 'editProfile':
+                document.getElementById('profileContent').innerHTML = data;
+                break;
+            case 'privacyContent':
+                document.getElementById('privacyContent').innerHTML = data;
+                break;
 
-    switch (type) {
-        case 'editProfile':
-            content = getEditProfileContent();
-            break;
-        case 'display':
-            content = getDisplayContent();
-            break;
-        case 'logOut':
-            content = getLogOutContent();
-            break;
-        case 'privacy':
-            content = getPrivacyContent();
-            break;
-        case 'notification':
-            content = getNotificationContent();
-            break;
+            case 'display':
+                document.getElementById('displayContent').innerHTML = data;
+                break;
+            case 'logOut':
+                document.getElementById('logOutContent').innerHTML = data;
+                break;
+            case 'notification':
+                document.getElementById('notificationSettings').innerHTML = data;
+                break;
+        }
+
+    })
+    .catch(error => console.error('Error loading content:', error));
+
+
         // Add more cases for other buttons
 
-        default:
-            content = "Default content";
-    }
-
-    document.getElementById('content').innerHTML = content;
-}
-
-function showChannel(type)
-{
-    var channel = "";
-
-    switch (type) {
-        case 'channel1':
-            channel = "Channel 1";
-            break;
-
-        default:
-            channel = "Default Channel";
-    }
-
-    document.getElementById('channel').innerHTML = channel;
-}
-
-function getPrivacyContent()
-{
-    return `
-    <div class="settings>
-        <div id="privacyContent">
-            <h2>Privacy and Accessibility</h2>
-            <span class="dot"></span>
-
-            <form>
-                <label for="changePassword">Change Password:</label>
-                <input type="text" id="changePassword" name="changePassword"><br>
-
-                <label for="confirmPassword">Confirm Password:</label>
-                <input type="password" id="confirmPassword" name="confirmPassword"><br>
-            
-            
-                <button type="deleteAccount">Delete Account?</button>
-            </form>
-        </div>
-    </div>
-    `;
-}
-
-
-
-function getEditProfileContent() {
-    return `
-    <div class="settings>
-        <div id="profileContent">
-            <h2>Edit Profile</h2>
-            <span class="dot"></span>
-
-            <form>
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username"><br>
-
-                <label for="displayName">Display Name:</label>
-                <input type="text" id="displayName" name="displayName"><br>
-            
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email"><br>
-
-                <label for="birthday">Birthday:</label>
-                <input type="date" id="birthday" name="birthday"><br>
-            
-                <!-- Add more form fields as needed -->
-            
-                <button type="submit">Save Changes</button>
-            </form>
-        </div>
-    </div>
-    `;
-}
-
-function getDisplayContent() {
-    return `
-    <div class="settings>
-        <div id="displayContent">
-            <h2>Display Settings</h2>  
-            <label>
-                <input type="checkbox" id="darkMode"> Dark Mode
-            </label><br>
-
-            <label for="color">Color Scheme: </label>
-            <input type="color" id="color" name="color"><br>
-
-            
-            <!-- Add more display settings as needed -->
-            
-        </div>
-    </div>
-    `;
-}
-
-function getNotificationContent()
-{
-    return `
-    <div class="settings>
-    <div id="notificationContent">
-        <h2>Notification Settings</h2>  
-        <label>
-            <input type="checkbox" id="notifications"> Notifications Enabled
-        </label><br>
-
-        <label for="timeout"> Notification Time Out: </label>
-        <input type="time" id="timeout" name="timeout"><br>
-        
-        <!-- Add more display settings as needed -->
-        
-    </div>
-</div>
-    
-    `;
-}
-
-function getLogOutContent()
-{
-    return `
-    <div class="settings>
-        <div id="logOutContent">
-            <h2> Are you sure?</h2>
-            <li>
-                <a href="./login.html">
-                <button class="decisions" id="yes">Yes</button>
-                </a>
-                <a href="./main.html">
-                    <button class="decisions" id="yes">No</button>
-                </a>
-            </li>
-        </div>
-    </div>
-    `;
 }
 
 var modal = document.getElementById("channelModal");
