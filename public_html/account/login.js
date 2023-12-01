7fc5f5dcd77030db1997427d114ad0cac288f786
Claire Lodermeier <clairelodermeier@Claires-MacBook-Pro.local>
2023-12-01 12:36:31 -0700

    created separate js files for login and create to get rid of some errors, updated html scripts

--- public_html/account/login.js
+++ public_html/account/login.js
@@ -1,3 +1,5 @@
+
+
 function login() {
   let us = document.getElementById('username').value;
   let pw = document.getElementById('password').value;
@@ -20,72 +22,3 @@ function login() {
   });
 }
 
-//create user function
-function CreateUser() {
-  var name = document.getElementById('username').value;
-  var pass = document.getElementById('password').value;
-  var DoB = document.getElementById('birthdate').value;
-  var email = document.getElementById('email').value;
-
-  var imgFile = document.getElementById('profilePic').files[0];
-  var formData = new FormData();
-  formData.append('photo', imgFile, imgFile.name);
-
-  // Create the fetch request for the image
-  let imgUrl = '/upload';
-
-  let imgP = fetch(imgUrl, {
-    method: 'POST',
-    body: formData,
-  });
-  imgP.then((r) => {
-    return r.json();
-  }).then(async (idObj) => {
-    var userObj = { n: name, p: pass, d: DoB, e: email, i: idObj };
-    let url = '/account/create/';
-    let response = await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(userObj),
-      headers: { 'Content-Type': 'application/json' }
-    });
-    alert(response.text());
-  });
-
-
-
-}
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  const passwordInput = document.getElementById('password');
-  const confirmPasswordInput = document.getElementById('confirmPassword');
-  const passwordMatchMessage = document.getElementById('passwordMatchMessage');
-
-
-  function checkPasswordMatch() {
-    const password = passwordInput.value;
-    const confirmPassword = confirmPasswordInput.value;
-
-    if (password === confirmPassword) {
-      passwordMatchMessage.textContent = 'Passwords match!';
-      passwordMatchMessage.style.color = 'green';
-    } else {
-      passwordMatchMessage.textContent = 'Passwords do not match';
-      passwordMatchMessage.style.color = 'red';
-    }
-  }
-  function handleFormSubmit(event) {
-    const password = passwordInput.value;
-    const confirmPassword = confirmPasswordInput.value;
-
-    if (password !== confirmPassword) {
-      event.preventDefault(); // Prevent form submission if passwords don't match
-      alert('Passwords do not match. Please check again.');
-    }
-  }
-
-  // Add event listeners
-  confirmPasswordInput.addEventListener('input', checkPasswordMatch);
-  registrationForm.addEventListener('submit', handleFormSubmit);
-
-});
